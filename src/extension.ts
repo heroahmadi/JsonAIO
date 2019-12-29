53f99e2f732c96ae19d002da3894832bda7812c5
Akbar Ahmadi <hero.lionel@gmail.com>
2019-12-29 22:45:30 +0700

    handle if selection is empty

--- src/extension.ts
+++ src/extension.ts
@@ -10,11 +10,17 @@ export function activate(context: vscode.ExtensionContext) {
 			let document = editor.document;
 			let selection = editor.selection;
 
+			if(selection.isEmpty){
+				selection = new vscode.Selection(document.positionAt(0), document.positionAt(document.getText().length));
+			}
+
 			let jsonString = document.getText(selection);
 			let quotedString = "'" + jsonString + "'";
 			editor.edit(editBuilder => {
 				editBuilder.replace(selection, quotedString);
 			})
+			
+			editor.selection = new vscode.Selection(document.positionAt(0), document.positionAt(0));
 		}
 	});
 
